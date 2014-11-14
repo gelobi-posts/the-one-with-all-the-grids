7b00282a9e6634d450a0029dd05080b90866eeb5
unknown <aria.minaei@gmail.com>
2014-11-14 18:20:05 +0330

    Add an incredibly naiive counter to the update hub

--- updateHub2/index.php
+++ updateHub2/index.php
@@ -1,5 +1,7 @@
 <?php
 
+file_put_contents('./counter', '0', FILE_APPEND);
+
 // Allow from any origin
 if (isset($_SERVER['HTTP_ORIGIN'])) {
 	header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
